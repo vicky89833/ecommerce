a3a68423d6b8e95a26f76b6c0d531355fcee60f3
vicky kumar maurya <113588950+vicky89833@users.noreply.github.com>
2024-10-19 21:03:18 +0530

    Update settings.py

--- myecommerce/myecommerce/settings.py
+++ myecommerce/myecommerce/settings.py
@@ -135,8 +135,8 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER='maurya.8@iitj.ac.in'
-EMAIL_HOST_PASSWORD='zfbokphhklkcmwnd'
+EMAIL_HOST_USER='your_email'
+EMAIL_HOST_PASSWORD='Password'
 
 
 
